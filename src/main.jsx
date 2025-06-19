53ce6c24e7a8d28f37e2731db47e519b04894582
Richard Cesar Alexandre de Oliveira <richardcesar911@gmail.com>
2025-06-19 18:44:48 -0300

    Atualizacao de componente

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import Trilhas from './pages/Trilhas/Trilhas';
+import Guias from './pages/Guias/Guias'
+import Parceiros from './pages/Parceiros/Parceiros'
+import Plano from './pages/Plano/Plano'
+import Header from './components/Header/Header'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Trilhas />
+    <Header />
   </StrictMode>,
 );
