626a602b37f1801ee95e6b590de2adc9579c4bd1
Dev-Alexandre-Ferreira <Dev.alexandre1@outlook.comgit config --global user.name Dev-Alexandre-Ferreira>
2025-06-20 17:39:15 -0300

    teste

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import Trilhas from './pages/Trilhas/Trilhas';
+import Guias from './pages/Guias/Guias';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Trilhas />
+    <Guias />
   </StrictMode>,
 );
