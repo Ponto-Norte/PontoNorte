c56c0a15288ca6235c3183f6a8416c76b436b7f0
Richard Cesar Alexandre de Oliveira <richardcesar911@gmail.com>
2025-06-21 17:10:19 -0300

    Header User Logado

---
+++ src/components/HeaderLogado/HeaderLogado.jsx
@@ -0,0 +1,67 @@
+import '../HeaderLogado/HeaderLogado.css';
+import { Link } from 'react-router-dom';
+import pnLogo from '../../assets/pnLogo.svg';
+
+function HeaderLogado() {
+  return (
+    <div>
+      <header className="pnHeader">
+        <div className="pnHeaderContainer">
+          <Link to="/">
+            <img src={pnLogo} className="logo" alt="" />
+          </Link>
+
+          <div className="pnHeaderPesquisa">
+            <input
+              type="text"
+              className="pnHeaderBarraPesquisa"
+              placeholder="Pesquise sua trilha aqui!"
+            />
+
+            <button>
+              <i className="bxr  bx-search"></i>
+            </button>
+          </div>
+
+          <nav className="pnHeaderNav">
+            <nav aria-label="navegação primaria">
+              <ul className="pnHeaderNavMenu">
+                <li>
+                  <Link className="pnHeaderLink" to="/">
+                    Início
+                  </Link>
+                </li>
+                <li>
+                  <Link className="pnHeaderLink" to="/">
+                    Trilhas
+                  </Link>
+                </li>
+                <li>
+                  <Link className="pnHeaderLink" to="/">
+                    Guias
+                  </Link>
+                </li>
+                <li>
+                  <Link className="pnHeaderLink" to="/">
+                    Parceiros
+                  </Link>
+                </li>
+                <li>
+                  <Link className="pnHeaderLink" to="/">
+                    Planos
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          </nav>
+
+          <div className="pnHeaderPerfil">
+            <button><img width="24" height="24" src="https://img.icons8.com/material/24/user-male-circle--v1.png" alt="user-male-circle--v1"/></button>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default HeaderLogado;
--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,12 @@ import { BrowserRouter } from 'react-router-dom';
 import Parceiros from './pages/Parceiros/Parceiros';
 import Guias from './pages/Guias/Guias';
 import Plano from './pages/Plano/Plano';
+import HeaderLogado from './components/HeaderLogado/HeaderLogado';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <Guias />
-      <Plano />
+      <HeaderLogado />
     </BrowserRouter>
   </StrictMode>,
 );
