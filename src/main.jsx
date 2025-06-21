1ce19f503f99347b3b2502345fd7f207e2c6dd55
Richard Cesar Alexandre de Oliveira <richardcesar911@gmail.com>
2025-06-21 15:58:55 -0300

    atts

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import Parceiros from './pages/Parceiros/Parceiros';
+import Guias from './pages/Guias/Guias';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <Parceiros />
+      <Guias />
     </BrowserRouter>
   </StrictMode>,
 );
