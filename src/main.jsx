c6036d6aa4f77b528907026ac9e30c925b8fb72c
Richard Cesar Alexandre de Oliveira <richardcesar911@gmail.com>
2025-06-20 22:28:30 -0300

    Atualização Guias

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import Trilhas from './pages/Trilhas/Trilhas';
+import Guias from './pages/Guias/Guias';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Trilhas />
+    <Guias />
   </StrictMode>,
 );
