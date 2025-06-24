bec39e2b82f802c4c4f31f8e24120e216beae70b
Richard Cesar Alexandre de Oliveira <richardcesar911@gmail.com>
2025-06-24 00:55:31 -0300

    Tela Login

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,12 @@ import Parceiros from './pages/Parceiros/Parceiros';
 import Guias from './pages/Guias/Guias';
 import Plano from './pages/Plano/Plano';
 import HeaderLogado from './components/HeaderLogado/HeaderLogado';
+import LoginTela from './pages/Login/Login';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <HeaderLogado />
+      <LoginTela />
     </BrowserRouter>
   </StrictMode>,
 );
