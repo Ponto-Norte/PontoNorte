46a0e085331f0854a0edd5e2da4767b18080f85f
Richard Cesar Alexandre de Oliveira <richardcesar911@gmail.com>
2025-06-25 11:41:29 -0300

    Pasta Fotos Home

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import Parceiros from './pages/Parceiros/Parceiros';
-import Guias from './pages/Guias/Guias';
-import Plano from './pages/Plano/Plano';
-import HeaderLogado from './components/HeaderLogado/HeaderLogado';
-import LoginTela from './pages/Login/Login';
-import RegistroOpcoesTela from './pages/RegistroOpcoes/RegistroOpcoes';
-import RegistroTrilheiroTela from './pages/RegistroTrilheiro/RegistroTrilheiro';
-import RegistroTrilheiroSucessoTela from './pages/RegistroTrilheiroSucesso/RegistroTrilheiroSucesso';
-import RegistroGuiaTela from './pages/RegistroGuia/RegistroGuia';
-import RegistroGuiaEnderecoTela from './pages/RegistroGuiaEndereco/RegistroGuiaEndereco';
-import RegistroGuiaBancariosTela from './pages/RegistroGuiaBancarios/RegistroGuiaBancarios';
 import RegistroGuiaSucessoTela from './pages/RegistroGuiaSucesso/RegistroGuiaSucesso';
 
 
